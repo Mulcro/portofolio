e95d2bf69a5ec0b2de40de36cec45439d7132ca2
Mulero Alamou <Mulero.alamou@gmail.com>
2024-04-13 20:25:17 -0700

    Reverted animations as they weren't the cause of webgl crashes

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -16,21 +16,19 @@ export const textVariant = (delay) => {
     };
   };
   
-  export const fadeIn = (direction, delay, duration) => {
-    const isHorizontal = direction === "left" || direction === "right";
-    const distance = isHorizontal ? "100vw" : "100vh";
-    
+  export const fadeIn = (direction, type, delay, duration) => {
     return {
       hidden: {
+        x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+        y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
         opacity: 0,
-        x: isHorizontal ? (direction === "left" ? distance : `-${distance}`) : 0,
-        y: isHorizontal ? 0 : (direction === "up" ? distance : `-${distance}`)
       },
       show: {
-        opacity: 1,
         x: 0,
         y: 0,
+        opacity: 1,
         transition: {
+          type: type,
           delay: delay,
           duration: duration,
           ease: "easeOut",
